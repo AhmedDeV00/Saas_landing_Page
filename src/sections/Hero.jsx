b6cc780b3acc2a72a9500bcb3728845d1ec4ec95
AhmedDeV00 <yy3288996@gmail.com>
2025-09-04 18:31:09 +0100

    secont commit

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <div>Hero</div>
+        <section className='relative border-2 border-amber-400'>
+
+        </section>
     )
 }
 
