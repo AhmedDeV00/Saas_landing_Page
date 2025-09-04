e07245b7e1627002d0f9b4049c5b80b033f44fb7
AhmedDeV00 <yy3288996@gmail.com>
2025-09-04 18:34:08 +0100

    use element feature from react-scroll

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Element } from 'react-scroll'
 
 const Hero = () => {
     return (
-        <section className='relative border-2 border-amber-400'>
+        <section className='relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32 border-2 border-amber-400'>
+            <Element name='hero'></Element>
 
         </section>
     )
