871ba3ca144f97da0b39d0e045bb6db620a5f243
AhmedDeV00 <yy3288996@gmail.com>
2025-09-11 19:46:46 +0100

    second commi

---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+export const features = [
+    {
+        id: "0",
+        icon: '/imagees/feature-1.png',
+        title: "Feature 1",
+        text: "Text 1",
+        button: {
+            icon: '/images/magictouch.svg',
+            title: 'Watch the demo'
+        }
+    },
+    {
+        id: "1",
+        icon: '/imagees/feature-2.png',
+        title: "Feature 2",
+        text: "Text 2",
+        button: {
+            icon: '/images/magictouch.svg',
+            title: 'Watch the demo'
+        }
+    }
+]
--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { Element } from 'react-scroll'
+import { features } from '../constants/index.js'
 
 const Features = () => {
     return (
         <section>
             <Element className="features" >
                 <div className="container">
-                    <div className="relative flex"></div>
+                    <div className="relative flex">
+                        {features.map((feature) => (
+                            <div key={feature.id}>{feature.title}</div>
+                        ))}
+                    </div>
                 </div>
             </Element>
         </section>
