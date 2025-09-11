d13a9661116779f2bf79a2bb42cbbaadc9669085
AhmedDeV00 <yy3288996@gmail.com>
2025-09-11 19:35:58 +0100

    first commit

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Features = () => {
     return (
-        <div>Features</div>
+        <section>
+            <Element className="features" >
+                <div className="container">
+                    <div className="relative flex"></div>
+                </div>
+            </Element>
+        </section>
     )
 }
 
